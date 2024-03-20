f60f62276ae921df9b05da91cfc691165815daf4
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-03-20 01:54:56 -0700

    ESM Source Folder
    The source folder is now build using TS as well! It's still output as CJS for the time being though. I have to fix the JSDoc type placement now that it no longer sets things to the module exports object, which make it more complex to type things out (it can't be declared in a comment above).

--- src/lz4.js
+++ src/lz4.js
@@ -8,8 +8,8 @@
 // - HC support (better search algorithm)
 // - Tests/benchmarking
 
-var xxhash = require('./xxh32.js');
-var util = require('./util.js');
+import * as xxhash from './xxh32.js';
+import * as util from './util.js';
 
 // Constants
 // --
@@ -148,12 +148,12 @@ function sliceArray (array, start, end) {
 // --
 
 // Calculates an upper bound for lz4 compression.
-exports.compressBound = function compressBound (/** @type {number} */ n) {
+export function compressBound (/** @type {number} */ n) {
   return (n + (n / 255) + 16) | 0;
 };
 
 // Calculates an upper bound for lz4 decompression, by reading the data.
-exports.decompressBound = function decompressBound (/** @type {number[] | Uint8Array} */ src) {
+export function decompressBound (/** @type {number[] | Uint8Array} */ src) {
   var sIndex = 0;
 
   // Read magic number
@@ -218,10 +218,10 @@ exports.decompressBound = function decompressBound (/** @type {number[] | Uint8A
 };
 
 // Creates a buffer of a given byte-size, falling back to plain arrays.
-exports.makeBuffer = makeBuffer;
+export { makeBuffer };
 
 // Decompresses a block of Lz4.
-exports.decompressBlock = function decompressBlock (/** @type {number[] | Uint8Array} */ src, /** @type {number[] | Uint8Array} */ dst, /** @type {number} */ sIndex, /** @type {number} */ sLength, /** @type {number} */ dIndex) {
+export function decompressBlock (/** @type {number[] | Uint8Array} */ src, /** @type {number[] | Uint8Array} */ dst, /** @type {number} */ sIndex, /** @type {number} */ sLength, /** @type {number} */ dIndex) {
   var mLength, mOffset, sEnd, n, i;
   var hasCopyWithin = dst.copyWithin !== undefined && dst.fill !== undefined;
 
@@ -295,7 +295,7 @@ exports.decompressBlock = function decompressBlock (/** @type {number[] | Uint8A
 };
 
 // Compresses a block with Lz4.
-exports.compressBlock = function compressBlock (/** @type {number[] | Uint8Array} */ src, /** @type {number[] | Uint8Array} */ dst, /** @type {number} */ sIndex, /** @type {number} */ sLength, /** @type {number[] | Uint32Array} */ hashTable) {
+export function compressBlock (/** @type {number[] | Uint8Array} */ src, /** @type {number[] | Uint8Array} */ dst, /** @type {number} */ sIndex, /** @type {number} */ sLength, /** @type {number[] | Uint32Array} */ hashTable) {
   var mIndex, mAnchor, mLength, mOffset, mStep;
   var literalCount, dIndex, sEnd, n;
 
@@ -411,7 +411,7 @@ exports.compressBlock = function compressBlock (/** @type {number[] | Uint8Array
 };
 
 // Decompresses a frame of Lz4 data.
-exports.decompressFrame = function decompressFrame (/** @type {number[] | Uint8Array} */ src, /** @type {number[] | Uint8Array} */ dst) {
+export function decompressFrame (/** @type {number[] | Uint8Array} */ src, /** @type {number[] | Uint8Array} */ dst) {
   var useBlockSum, useContentSum, useContentSize, descriptor;
   var sIndex = 0;
   var dIndex = 0;
@@ -477,7 +477,7 @@ exports.decompressFrame = function decompressFrame (/** @type {number[] | Uint8A
       }
     } else {
       // Decompress into blockBuf
-      dIndex = exports.decompressBlock(src, dst, sIndex, compSize, dIndex);
+      dIndex = decompressBlock(src, dst, sIndex, compSize, dIndex);
       sIndex += compSize;
     }
   }
@@ -491,7 +491,7 @@ exports.decompressFrame = function decompressFrame (/** @type {number[] | Uint8A
 };
 
 // Compresses data to an Lz4 frame.
-exports.compressFrame = function compressFrame (/** @type {number[] | Uint8Array} */ src, /** @type {number[] | Uint8Array} */ dst) {
+export function compressFrame (/** @type {number[] | Uint8Array} */ src, /** @type {number[] | Uint8Array} */ dst) {
   var dIndex = 0;
 
   // Write magic number.
@@ -519,7 +519,7 @@ exports.compressFrame = function compressFrame (/** @type {number[] | Uint8Array
     var compSize = 0;
     var blockSize = remaining > maxBlockSize ? maxBlockSize : remaining;
 
-    compSize = exports.compressBlock(src, blockBuf, sIndex, blockSize, hashTable);
+    compSize = compressBlock(src, blockBuf, sIndex, blockSize, hashTable);
 
     if (compSize > blockSize || compSize === 0) {
       // Output uncompressed.
@@ -555,14 +555,14 @@ exports.compressFrame = function compressFrame (/** @type {number[] | Uint8Array
 // Decompresses a buffer containing an Lz4 frame. maxSize is optional; if not
 // provided, a maximum size will be determined by examining the data. The
 // buffer returned will always be perfectly-sized.
-exports.decompress = function decompress (/** @type {number[] | Uint8Array} */ src, /** @type {number | undefined} */ maxSize) {
+export function decompress (/** @type {number[] | Uint8Array} */ src, /** @type {number | undefined} */ maxSize) {
   var dst, size;
 
   if (maxSize === undefined) {
-    maxSize = exports.decompressBound(src);
+    maxSize = decompressBound(src);
   }
-  dst = exports.makeBuffer(maxSize);
-  size = exports.decompressFrame(src, dst);
+  dst = makeBuffer(maxSize);
+  size = decompressFrame(src, dst);
 
   if (size !== maxSize) {
     dst = sliceArray(dst, 0, size);
@@ -574,15 +574,15 @@ exports.decompress = function decompress (/** @type {number[] | Uint8Array} */ s
 // Compresses a buffer to an Lz4 frame. maxSize is optional; if not provided,
 // a buffer will be created based on the theoretical worst output size for a
 // given input size. The buffer returned will always be perfectly-sized.
-exports.compress = function compress (/** @type {number[] | Uint8Array} */ src, /** @type {number | undefined} */ maxSize) {
+export function compress (/** @type {number[] | Uint8Array} */ src, /** @type {number | undefined} */ maxSize) {
   var dst, size;
 
   if (maxSize === undefined) {
-    maxSize = exports.compressBound(src.length);
+    maxSize = compressBound(src.length);
   }
 
-  dst = exports.makeBuffer(maxSize);
-  size = exports.compressFrame(src, dst);
+  dst = makeBuffer(maxSize);
+  size = compressFrame(src, dst);
 
   if (size !== maxSize) {
     dst = sliceArray(dst, 0, size);
